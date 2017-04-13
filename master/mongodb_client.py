1e9f7092f4e929b98a7ceb86d139193555bd98dd
vance <liyannois@gmail.com>
2017-04-14 01:30:42 +0800

    start services

---
+++ master/mongodb_client.py
@@ -0,0 +1,4 @@
+from pymongo import MongoClient
+
+client = MongoClient()  # 默认连接 localhost:27017
+Mdb = client.xueqiu
---
+++ master/redis_client.py
@@ -0,0 +1,2 @@
+import redis
+r_client = redis.Redis(host="localhost",port=6379,db=0)
